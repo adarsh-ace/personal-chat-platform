84298734f94e1af381171f6586926fc08da9b813
adarsh-ace <adarshsinght56@gmail.com@gmail.com>
2026-01-17 15:47:47 +0530

    phase-1: initial spring boot project setup

---
+++ personal-chat-platform/src/main/java/com/ace/personal_chat_platform/PersonalChatPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.ace.personal_chat_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalChatPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalChatPlatformApplication.class, args);
+	}
+
+}
---
+++ personal-chat-platform/src/test/java/com/ace/personal_chat_platform/PersonalChatPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ace.personal_chat_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalChatPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
